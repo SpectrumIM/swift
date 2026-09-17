5cb357142693d718baff4b387e394ea22da83373
Vitaly Takmazov <vitalyster@gmail.com>
2026-09-17 07:47:00 +0300

    Port BuildTools version scripts to Python 3
    GetBuildVersion.py, DebianizeVersion.py and UpdateDebianChangelog.py used Python 2 print statements and fail outright under Python 3 (no python2 is available on Debian trixie). Take the already-ported Python 3 versions from the spectrum_dev branch (70cf05af7d51).

--- BuildTools/UpdateDebianChangelog.py
+++ BuildTools/UpdateDebianChangelog.py
@@ -20,7 +20,7 @@ if m :
 if project == "" :
     project="swift-im"
 
-if "dev" in version or "alpha" in version :
+if "dev" in version :
     distribution = "development"
 elif "beta" in version or "rc" in version :
     distribution = "beta development"
@@ -38,3 +38,4 @@ if last_version != version :
     changelog.write("\n")
     changelog.write(changelog_data)
     changelog.close()
+
